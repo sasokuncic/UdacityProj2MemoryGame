9219523775b85938316487d700c3bc511f6fb437
sasokuncic <saso.kuncic@gmail.com>
2018-04-25 10:37:21 +0200

    Styling changes

--- js/app.js
+++ js/app.js
@@ -2,8 +2,8 @@
 /* **************************************************
   * Google Udacity FEND Lesson 24, Memory Game
   ************************************************** */
-// @description game initialization 
- // 
+// @description: Game initialization 
+
 let card = document.getElementsByClassName('card');
 let cards = [...card];
 // prepare the deck of all cards in the game
@@ -17,9 +17,9 @@ let cardsMatchedNum = 0;
 let cardMoves = 0;
 let cardMovesCounter = document.querySelector('.moves');
 
- // rating - stars list
- let stars = document.querySelectorAll(".stars li");
- let starsNumber = 3;
+// rating - stars list
+let stars = document.querySelectorAll(".stars li");
+let starsNumber = 3;
 
 // timer
 let second = 0;
@@ -37,7 +37,7 @@ var span = document.getElementsByClassName("close")[0];
 document.body.onload = startGame();
 
 // @description new game start
-function startGame(){
+function startGame() {
     cards = shuffle(cards);
     // remove existing deck DOM
     deck.innerHTML = '';
@@ -45,7 +45,7 @@ function startGame(){
     // attach each card to the deck
     cards.forEach.call(cards, function(card) {
         card.classList.remove('show', 'open', 'match', 'disabled');
-        //card.classList.add('open'); // DBG (debug statement)
+        // card.classList.add('open'); // DBG (debug statement)
         deck.appendChild(card);
     }); 
     cardsOpened = [];
@@ -63,14 +63,16 @@ function startGame(){
 
     // reset timer
     second = 0;
-    minute = 0; 
+    minute = 0;
     hour = 0;
     timer.innerHTML = minute+'mins '+second+'secs';
     clearInterval(interval);
 }
 
 // @description Deck cards shuffle function
-// From http://stackoverflow.com/a/2450976
+// @param Array to be shuffled
+// @returns Shuffled input array
+// Note: Code from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
 
@@ -91,25 +93,25 @@ deck.addEventListener('click', function (event) {
 
     // Do not allow the grid section itself to be selected; only select divs inside the grid
     if (clicked.nodeName === 'UL') {
-    	console.log('Opened card\'s frame clicked');	// DBG
+    	// console.log('Opened card\'s frame clicked');	// DBG
     	return; 
     }
   
     if (cards.length > cardsMatchedNum) {
         cardsOpened.push(clicked);
         cardDisplay(clicked);
-        cardCheck();        
+        cardCheck();
         // console.log('Type: ' + clicked.type + ' NodeName: ' + clicked.nodeName);  // DBG
     };
  });
 
  // @description Display selected card
-function cardDisplay(cardSelected){
+function cardDisplay(cardSelected) {
     cardSelected.classList.add('open', 'show', 'disabled');
 }
 
- // @description count player's moves
-function cardCheck(){
+ // @description Checks player's move
+function cardCheck() {
 
     // check for matching
     if (cardsOpened.length == 2) {
@@ -118,7 +120,7 @@ function cardCheck(){
             cardsMatched();
             cardsMatchedNum++;
             if (cards.length == (cardsMatchedNum * 2)) {
-                console.log ('All cards matched!'); // DBG, TODO - open modal window
+                // console.log ('All cards matched!'); // DBG
                 clearInterval(interval);
                 openModalWindow();
             }
@@ -129,33 +131,33 @@ function cardCheck(){
 }
 
 // @description Update number of moves, start timer on the first move, update rating
-function updateMovesAndRating(){
+function updateMovesAndRating() {
 
     // increment and update moves counter
     cardMoves++;
     cardMovesCounter.innerHTML = cardMoves;
 
     //start timer on the first move
-    if(cardMoves == 1){
+    if (cardMoves == 1) {
         second = 0;
-        minute = 0; 
+        minute = 0;
         hour = 0;
         startTimer();
     }
 
     // update rating according to number of moves
     starsNumber = 3;
-    if (cardMoves > 12 && cardMoves <= 24){
+    if (cardMoves > 12 && cardMoves <= 24) {
         starsNumber = 2;
-        for( i= 0; i < 3; i++){
-            if(i > 1){
+        for ( let i = 0; i < 3; i++) {
+            if (i > 1) {
                 stars[i].style.visibility = "collapse";
             }
         }
-    } else if (cardMoves > 24){
+    } else if (cardMoves > 24) {
         starsNumber = 1;
-        for( i= 0; i < 3; i++){
-            if(i > 0){
+        for ( let i = 0; i < 3; i++) {
+            if (i > 0) {
                 stars[i].style.visibility = "collapse";
             }
         }
@@ -163,14 +165,14 @@ function updateMovesAndRating(){
 }
 
 // @description Game Timer
-function startTimer(){
-    interval = setInterval(function(){
+function startTimer() {
+    interval = setInterval(function() {
         second++;
-        if(second == 60){
+        if (second == 60) {
             minute++;
             second=0;
         }
-        if(minute == 60){
+        if (minute == 60) {
             hour++;
             minute = 0;
         }
@@ -179,15 +181,15 @@ function startTimer(){
     },1000);
 }
 
-// @description 
+// @description: Cards matching manipulation
 // Note: for the animation of matched and not matched cards used Daniel's Eden, Animate CSS, 
-//       from https://github.com/daneden/animate.css/blob/master/README.md 
-function cardsMatched(){
+// Note: used animation library described at https://github.com/daneden/animate.css/blob/master/README.md 
+function cardsMatched() {
     console.log('cardsMatched'); // DBG
     cardsOpened[0].classList.add('match', 'animated', 'tada');
     cardsOpened[1].classList.add('match', 'animated', 'tada');
     cardsDisable();
-    setTimeout(function(){
+    setTimeout(function() {
         cardsOpened[0].classList.remove('open', 'show', 'notmatched', 'animated', 'tada');
         cardsOpened[1].classList.remove('open', 'show', 'notmatched', 'animated', 'tada');
         cardsEnable();
@@ -195,13 +197,13 @@ function cardsMatched(){
     },800);
 }
 
-// @description 
-function cardsNotMatched(){
-    console.log('cardsNotMatched');  // DBG
+// @description: Manipulation of cards which not match
+function cardsNotMatched() {
+    // console.log('cardsNotMatched');  // DBG
     cardsOpened[0].classList.add('notmatched', 'animated', 'wobble');
     cardsOpened[1].classList.add('notmatched', 'animated', 'wobble');
     cardsDisable();
-    setTimeout(function(){
+    setTimeout(function() {
         cardsOpened[0].classList.remove('open', 'show', 'notmatched', 'animated', 'wobble');
         cardsOpened[1].classList.remove('open', 'show', 'notmatched', 'animated', 'wobble');
         cardsEnable();
@@ -209,10 +211,10 @@ function cardsNotMatched(){
     },900);
 }
 
-// @description Disable cards
+// @description: Disable cards
 // matched cards allready disabled
-function cardsDisable(){
-    console.log('cardsDisable');  // DBG
+function cardsDisable() {
+    // console.log('cardsDisable');  // DBG
     cards.forEach.call(cards, function(card) {
         if (!card.classList.contains('match')) {
             card.classList.add('disabled');
@@ -220,10 +222,10 @@ function cardsDisable(){
     }); 
 }
 
-// @description Enable cards
+// @description: Enable cards
 // all but matched
-function cardsEnable(){
-    console.log('cardsEnable');  // DBG
+function cardsEnable() {
+    // console.log('cardsEnable');  // DBG
     cards.forEach.call(cards, function(card) {
         if (!card.classList.contains('match')) {
             card.classList.remove('disabled');
@@ -231,22 +233,22 @@ function cardsEnable(){
     }); 
 }
 
-// @description Modal window 
+// @description: Congratulations popup - modal window 
 function openModalWindow() {
     modal.style.display = "block";
     document.getElementById("finalMoves").innerHTML = cardMoves;
     document.getElementById("finalStars").innerHTML = starsNumber;
 }
 
-// @description Close modal window by clicking icon (x) <span>
+// @description: Close modal window by clicking icon (x) <span>
 span.onclick = function() {
     modal.style.display = "none";
-    console.log('Close icon of the modal window selected');   // DBG
+    // console.log('Close icon of the modal window selected');   // DBG
 }
 
-// @description btn Play Again in modal window clicked
+// @description: Play Again button in modal window clicked
 function playAgain() {
     modal.style.display = "none";
-    console.log('Play again button clicked');   // DBG
+    // console.log('Play again button clicked');   // DBG
     startGame();
 }
